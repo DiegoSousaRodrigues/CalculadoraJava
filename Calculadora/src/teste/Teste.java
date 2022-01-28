b2365f9458b9fdb44482358e27bd711690e77b85
DiegoSousaRodrigues <diego.srodrigues2002@gmail.com>
2022-01-27 22:23:08 -0300

    Adição criada

--- Calculadora/src/teste/Teste.java
+++ Calculadora/src/teste/Teste.java
@@ -1,7 +1,13 @@
 package teste;
 
+import javax.swing.JOptionPane;
+
+import operation.Calculadora;
+
 public class Teste {
+    static Calculadora calc = new Calculadora();
+
     public static void main(String[] args) {
-        
+        JOptionPane.showMessageDialog(null, "Resultado adição: " + calc.adicao());
     }
 }
